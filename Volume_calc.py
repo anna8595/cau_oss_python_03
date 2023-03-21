e347167d645fe1b66f07852d4abac78be0690a45
anna8595 <128022523+anna8595@users.noreply.github.com>
2023-03-21 19:37:47 +0900

    Update Volume_calc.py
    택배요금 추가한 수정

--- Volume_calc.py
+++ Volume_calc.py
@@ -1,6 +1,17 @@
-widht = float(input("widht: "))
+width = float(input("widht: "))
 length = float(input("length: "))
 high = float(input("high: "))
 
-volume = widht * length * high 
+volume = width * length * high 
 print("The volume of the box is", volume)
+
+total_length = length + width + high
+if total_length <= 80 : 
+    print('택배요금은 5000원입니다.')
+elif total_length > 80 and total_length <= 100 :
+    print('택배요금은 8000원입니다.')
+elif total_length > 100 and total_length <= 120 :
+    print('택배요금은 10000원입니다.')
+elif total_length > 120 and total_length <= 160 :
+    print('택배요금은 13000원입니다.')
+else : print('택배 접수가 불가능한 크기입니다.')
